96233c9c43937448ddbe23b97d303ac633b9e0bb
Alex Peluso <alexander.peluso@gmail.com>
2021-06-07 16:34:11 -0700

    Add project files.

---
+++ Execise7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first;
+            int second;
+            int third;
+            int sum = 0;
+
+            Console.Write("Enter an integer: ");
+            first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second integer: ");
+            second = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a third integer: ");
+            third = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum of the three integers is " + sumitup(first, second, third));
+
+            Console.Write("Enter an integer: ");
+            first = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a second integer: ");
+            second = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a third integer: ");
+            third = Convert.ToInt32(Console.ReadLine());
+
+            sumitup(first, second, third, ref sum);
+
+            Console.WriteLine("The sum of the three integers is " + sum);
+
+        }
+
+        static int sumitup(int one, int two, int three)
+        {
+            return one + two + three;
+        }
+
+        static void sumitup(int one, int two, int three, ref int sum)
+        {
+            sum = one + two + three;
+        }
+    }
+}
---
+++ Exercise8/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] groceryitems = new string[5];
+
+            int j = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Enter grocery item " + i + ": ");
+                groceryitems[j] = Console.ReadLine();
+                j++;
+            }
+
+            string item;
+
+            Console.WriteLine();
+
+            Console.Write("Enter a substring to search for: ");
+            item = Console.ReadLine();
+
+            Console.WriteLine();
+
+            int occurances=0;
+
+            for (int i = 0; i < groceryitems.Length; i++)
+            {
+                if (groceryitems[i].Contains(item))
+                {
+                    occurances++;
+                }
+            }
+
+            Console.WriteLine("There are " + occurances + " that contain the substring \"" + item + "\"");
+
+        }
+    }
+}
